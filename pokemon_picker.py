61b88d5965a7a55dcba070663efea9995e5e8d9a
Tim Power <tjdpower@shaw.ca>
2023-12-07 08:51:06 -0700

    Added to pokemon type selection logic

--- pokemon_picker.py
+++ pokemon_picker.py
@@ -31,15 +31,17 @@ my_type_list = insert_pokemon_types()
 my_pokemon_list = insert_pokemon_data()
 
 selected_pokemon_type = ""
+second_selected_pokemon_type = ""
 
 #getting pokemon type from user
 while len(selected_pokemon_type) == 0:
     user_input = str(input("""
         
-        What type of pokemon are you fighting?
-        Type the beginning of that pokemone type and
-        hit enter to see if it's here.
-        """
+What type of pokemon are you fighting?
+Type the beginning of that pokemone type and
+hit enter to see if it's here.
+                           
+"""
     )).lower()
 
     #find pokemon type in the list, if it exists
@@ -54,4 +56,56 @@ while len(selected_pokemon_type) == 0:
     for pokemon in matches:
         print(pokemon)
     
-    
+    #Check if result has been narrowed down to one type of pokemon
+    if len(matches) == 1:
+        select_type = str(input(f"""
+The only matching type for the specified input is {matches[0]}.
+Do you want to look at {matches[0]} type pokemon?
+Enter 'y' for yes and 'n' for no.
+
+""")).lower()
+        
+        #some pokemon have two types, check if pokemon has a second type, then get input if needed
+        if select_type == 'y':
+            selected_pokemon_type = matches[0]
+            print(f"\nSelected pokemon type is {selected_pokemon_type}.\n")
+            second_type_check = str(input("""
+Some pokemon can have two types combined.
+Does your opponent have a second type?
+Enter 'y' for yes and 'n' for no.
+                                      
+""")).lower()
+            
+            if second_type_check == 'y':
+                
+                while len(second_selected_pokemon_type) == 0:
+                    second_user_input = str(input("""
+        
+What is the second type of the pokemon are you fighting?
+Type the beginning of that pokemone type and
+hit enter to see if it's here.
+                           
+"""
+    )).lower()
+                    second_matches = []
+                    type_list_head = my_type_list.get_head_node()
+                    while type_list_head is not None:
+                        if str(type_list_head.get_value()).startswith(second_user_input):
+                            second_matches.append(type_list_head.get_value())
+                        type_list_head = type_list_head.get_next_node()
+                    
+                    #print list of matching types
+                    for pokemon in second_matches:
+                        print(pokemon)
+                    
+                    #Check if result has been narrowed down to one type of pokemon
+                    if len(second_matches) == 1:
+                        second_select_type = str(input(f"""
+The only matching type for the specified input is {second_matches[0]}.
+Do you want to look at {second_matches[0]} type pokemon?
+Enter 'y' for yes and 'n' for no.
+
+""")).lower()
+                        if second_select_type == 'y':
+                            second_selected_pokemon_type = second_matches[0]
+                            print(f"\nSelected pokemon type is {selected_pokemon_type} and {second_selected_pokemon_type}.\n")
